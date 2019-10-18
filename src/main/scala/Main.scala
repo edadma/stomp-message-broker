3801a7a1616f9211aa6de202ca56e6a45a14845d
Edward A Maxedon, Sr <edadma@vinctus.com>
2019-10-18 17:48:40 -0400

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,12 @@ package xyz.hyperreal.stomp_message_broker
 
 import xyz.hyperreal.stomp_server.StompServer
 
+import scala.scalajs.js
+
 
 object Main extends App {
 
-    def authorize( headers: Map[String, String] ) = {
+    def authorize( headers: js.Dictionary[String] ) = {
       println( s"authorized: $headers" )
       true
     }
@@ -13,6 +15,6 @@ object Main extends App {
     val serverHostname = "192.168.0.117"
     val serverPort = 15674
     val serverPath = "/stomp"
-    val server = new StompServer( "STOMP Message Broker/1.0", serverHostname, serverPort, serverPath, authorize, true )
+    val server = new StompServer( "STOMP Message Broker/1.0", serverHostname, serverPort, serverPath, authorize, authorize, true )
 
 }
